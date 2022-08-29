79f1c7aa8e79170639985140bcf927e5f46d2db0
Ömer Faruk Akal <o.farukakal@gmail.com>
2022-08-29 16:26:03 +0300

    Last Version

--- Python_Basics.py
+++ Python_Basics.py
@@ -1,7 +1,6 @@
-"""
 print("Hello World") #print function
 
-
+"""
 name = "Ömer" #define variable
 print(name)
 
@@ -16,8 +15,7 @@ print(a.upper())
 print(a.lower())
 print(a.find("Efe")) #find command, give index of first occurance of search
 print(a.replace("Efendim", "Ali"))
-print("Efendim" in a) #check the word in variable
-
+print("Efendim" in a) #Checks if "Efendim" is included in the variable
 
 # Type Conversion
 birth_year = input("What is ur birth year: ")
@@ -73,4 +71,67 @@ while i <= 10:
     i += 1
 
     print("Bu gün Hava Soğuk.")
+
+
+# Lists
+names = ["Ömer", "Ahmet", "Mehmet", "Ali", "Veli"]
+print(names)
+print(names[0])
+names[3] = "Yavuz"
+print(names)
+print(names[0:3]) # Print out 0th to 3th
+
+names.append("Efe") # Add "Efe" to names list
+print(names)
+
+names.insert(0, "Osman") # Add "Osman" to 0th index of names list
+print(names)
+
+names.remove("Mehmet") # Remove "Mehmet" from names list
+print(names)
+
+print("Ahmet" in names) # Checks if "Ahmet" is included in the list
+
+print(len(names)) # Print number of the elements in list
+
+
+# For Loop
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+
+# range() Function
+nums = range(10) # nums limited with 10
+for number in nums:
+    print(number)
+
+
+nums_1 = range(5, 10) # nums limited with 5 between 10
+for number in nums_1:
+    print(number)
+
+
+nums_2 = range(5, 10, 2) # nums limited with 5 between 10 and numbers increase with plus in two
+for number in nums_2:
+    print(number)
+
+
+
+#Tuples
+#Tuples are similar to lists but tuples are cannot be changed
+asd = (1, 2, 3, 4, 5)
+# asd[3] = 2 code gives us this error "'tuple' object does not support item assignment"
+print(asd)
+
+
+#Functions
+def user_register():
+    fname = input("Name: ")
+    fsurname = input("Surname: ")
+    print("Wellcome " + fname + " " + fsurname + ". You are Registered Now.")
+
+print("Wellcome Please Register")
+user_register()
 """
