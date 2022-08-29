60c0558a9f4329e0ca7d76a8791f52dbcabd9137
Ömer Faruk Akal <o.farukakal@gmail.com>
2022-08-29 13:22:48 +0300

    Create Python_Basics.py

---
+++ Python_Basics.py
@@ -0,0 +1,75 @@
+"""
+print("Hello World") #print function
+
+
+name = "Ömer" #define variable
+print(name)
+
+
+name_1 = input("What is ur name? ") #input function
+print("Welcome " + name_1)
+
+
+# String 
+a = "Merhaba Efendim."
+print(a.upper())
+print(a.lower())
+print(a.find("Efe")) #find command, give index of first occurance of search
+print(a.replace("Efendim", "Ali"))
+print("Efendim" in a) #check the word in variable
+
+
+# Type Conversion
+birth_year = input("What is ur birth year: ")
+age = 2022 - int(birth_year) #change data type string to integer
+print("You are " + str(age)) #change data type int to str
+
+
+#basic calculator
+x = float(input("Enter first number = "))
+y = float(input("Enter second number = "))
+sum = x + y
+print("Result = " + str(sum))
+
+
+# Arithmetic Operators
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3) 
+print(10 ** 3)
+
+
+# Comparison & Logical Op 
+
+>
+>
+<=
+>=
+==
+!=
+
+and
+or
+not
+
+
+# IF & Else
+temp = int(input("Hava sıcaklığını giriniz. C° = "))
+if temp >= 25:
+    print("Hava Çok Sıcak.")
+elif 18 <= temp <= 25:
+    print("Hava Çok Güzel")
+else:
+    print("Bu gün Hava Soğuk.")
+"""
+
+
+# While Loop
+i = 1 
+while i <= 31:
+    print(i)
+    i += 1
+aliii
