3102d4315f25acdd454fc52465800cf8fc1b4a4f
Ömer Faruk Akal <o.farukakal@gmail.com>
2022-09-03 00:04:09 +0300

    Update
    Score Count

--- Rock Paper Scissor/RPS_Game.py
+++ Rock Paper Scissor/RPS_Game.py
@@ -1,11 +1,12 @@
 import random
 import time
 
-
 pc_won = "AI Won, Go and Work Hard."
 player_won = "You Won, How it Possible."
+player_score = 0
+pc_score = 0
 
-round = int(input("How Many Round You Wanna Play? 1-3-5 = "))
+round = int(input("How Many Round You Wanna Play? "))
 i = 1
 while i <= round:
     player_1 = input("Select Rock, Paper or Scissor : ").upper()
@@ -14,14 +15,20 @@ while i <= round:
 
     if player_1 == "rock" and pc == "paper":
         print(pc_won)
+        pc_score += 1
     elif player_1 == "paper" and pc == "scissor":
          print(pc_won)
+         pc_score += 1
     elif player_1 == "scissor" and pc =="rock":
          print(pc_won)
+         pc_score += 1
     elif player_1 == pc:
         print("Draw, Come and Fight with Me Again")
     else:
-            print(player_won)   
+            print(player_won)
+            player_score += 1
     i += 1
+print("AI Score : " + str(pc_score))
+print("Player Score : " + str(player_score))
 
-time.sleep(3)
+time.sleep(5)
